5d7c6fd4b882377321b05ca39cb7caa7a073e8a8
AxisGe0 <69110100+AxisGe0@users.noreply.github.com>
2023-04-02 10:44:58 +0530

    Update main.lua

--- qb-inventory/client/main.lua
+++ qb-inventory/client/main.lua
@@ -168,3 +168,37 @@ RegisterNetEvent('inventory:client:CheckWeapon', function(weaponName)
         currentWeapon = nil
     end
 end)
+
+local function HasItem(items, amount)
+    local isTable = type(items) == 'table'
+    local isArray = isTable and table.type(items) == 'array' or false
+    local totalItems = #items
+    local count = 0
+    local kvIndex = 2
+	if isTable and not isArray then
+        totalItems = 0
+        for _ in pairs(items) do totalItems += 1 end
+        kvIndex = 1
+    end
+    local PlayerData = QBCore.Functions.GetPlayerData()
+    for _, itemData in pairs(PlayerData.items) do
+        if isTable then
+            for k, v in pairs(items) do
+                local itemKV = {k, v}
+                if itemData and itemData.name == itemKV[kvIndex] and ((amount and itemData.amount >= amount) or (not isArray and itemData.amount >= v) or (not amount and isArray)) then
+                    count += 1
+                end
+            end
+            if count == totalItems then
+                return true
+            end
+        else -- Single item as string
+            if itemData and itemData.name == items and (not amount or (itemData and amount and itemData.amount >= amount)) then
+                return true
+            end
+        end
+    end
+    return false
+end
+
+exports("HasItem", HasItem)
